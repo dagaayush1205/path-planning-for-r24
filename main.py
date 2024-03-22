e5e86597c9f41393315099a50eb20164a562d622
ayushd <dagaayush1205@gmail.com>
2024-03-22 23:40:48 +0530

    setting up

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 #code starts here
 import matplotlib.pyplot as plt
 import numpy as np
-import matplot as mpl
+import pandas as pd 
+import matplotlib as mpl
+fig, ax = plt.subplots()
+df = pd.read_ods("Object.ods")
+df
+plt.scatter(df.x,df.y)
+plt.show()
