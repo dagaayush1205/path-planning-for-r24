64acb7f3c0c1dc9de0335046782a698879018fde
ayushd <dagaayush1205@gmail.com>
2024-03-23 10:48:23 +0530

    setting up

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #code starts here
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd 
 import matplotlib as mpl
+import pandas as pd
+from csv import writer
 fig, ax = plt.subplots()
-df = pd.read_ods("Object.ods")
-df
+df = pd.read_csv("point.csv")
+#working arena size is 10m x 4m
+# i have take a precision of 1cm and taking every grid to be 1000 x 400 resolution
+for i in range 1000:
+    for j in range 400:
+        list[j,i
 plt.scatter(df.x,df.y)
 plt.show()
