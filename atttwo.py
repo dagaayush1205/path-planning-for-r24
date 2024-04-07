908939deabe336fa0ba38e3e00213943ecff888b
ayushd <dagaayush1205@gmail.com>
2024-04-07 19:39:55 +0530

    new code

---
+++ atttwo.py
@@ -0,0 +1,13 @@
+import math
+import matplotlib.pyplot as plt
+
+def main():
+    area = [[0,0,0]]
+    """
+    0: new
+    1: open
+    -1: obstacle
+    """
+    playGround=[0,100,0,100]
+    # playGround[xmin,xmax,ymin,ymax]
+    
--- main.py
+++ main.py
@@ -1,144 +1,58 @@
-#code starts here
+import math
+from sys import maxsize
 import matplotlib.pyplot as plt
-import numpy as np
-import matplotlib as mpl
-#working arena size is 10m x 4m
-# i have take a precision of 1cm and taking every grid to be 1000 x 400 resolution
-class RRT:
-    grid = np.full((101,101,1),-1)
-    # the grid has other values including obstacle size which will range from 0 to 1
-    # it also has orientation of the grid, all of them have -1 by default in case that
-    # it remains unexplored, in simple terms, rover has give -1 to all unexplored part
-    # since everything that it must include must be in whole numbers
-    # format(x,y,z,orientation wrt to rover, cost)
-    #note for testing stage the area is 100x100units only
-    class node:
 
-        def __init__(self,x,y):
-            self.x = x
-            self.y = y
-            self.path_x = []
-            self.path_y = []
-            self.parent = None
+show animation = True
 
-        class AreaBounds:
-            def __init__(self,area):
-                self.xmin = float(area[0])
-                self.xmax = float(area[1])
-                self.ymin = float(area[2])
-                self.ymax = float(area[3])
-                #area([xmin,xmax,ymin,ymax])
+class State:
 
-        def __init__(self,
-                     start,
-                     goal,
-                     obstacle_list,
-                     rand_area,
-                     expand_dis=10.0,
-                     path_resolution=1,
-                     goal_sample_rate=5,
-                     max_iter=5000,
-                     play_area=[0,100,0,100],
-                     robot_dim=[5,5]):
-            """
-            start:start position[x,y]
-            goal: [x,y]
-            obstacleList:obstacle position
-            randArea: Random Sampling Area[miin,max]
-            play_area: boundry
-            robot_dim = robot dimensions[X x Y]
-            """
-            self.start = self.Node(start[0],start[1])
-            self.end = self.Node(goal[0],goal[1])
-            self.min_rand = rand_area[0]
-            self.max_rand = rand_area[1]
-            print("start"
-            if play_area is not None:
-                self.play_area = self.AreaBounds(play_area)
-            else:
-                self.play_area = None
-            self.expand_dis = expand_dis
-            self.path_resolution = path_resolution
-            self.goal_sample_rate = goal_sample_rate
-            self.max_iter = max_iter
-            self.obstacle_list = obstacle_list
-            self.node_list = []
-            self.robot_dim = robot_dim
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.parent = None
+        self.state = "."
+        self.t = "new"
+        self.h = 0
+        self.k = 0
 
-        def planning(self, animation=True):
-            self.node_list = [self.start]
-            for i in range(self.amx_iter):
-                rnd_node = self.get_random_node()
-                nearest_ind = self.get_nearest_node_index(self.node_list,animation = True):
-                    rnd_node = self.get_random_node()
-                    nearest_ind = self.get_nearest_node_index(self.nodelist, rnd_node)
-                    nearest_node = self.node(nearest_node,rnd_node,self.expand_dis)
-                    if self.check_if_outside_play_area(new_node,self.play_area) and self.check_collision(new_node,self.obstacle_list,self.robot_dim):
-                        self.node_list.append(new_node)
-                    if animation and i % 5 == 0:
-                        self.draw_graph(rnd_node)
-                    if self.calc_dist_to_goal(self.node_list[-1].x,,self.node_list[-1].y) <= self.expand_dis:
-                        final_node = self.steer(self.node_list[-1], self.end, self.expand_dis)
-                        if self.check_collision(final_node, self.obstacle_list,self.robot_dim):
-                            return self.generate_final_course(len(self.node_list) - 1)
-                    if animation and i % 5:
-                        self.draw_graph(rnd_node)
+        def cost(self,state):
+            if self.state == "#" or state.state == "#":
+                return maxsize
+            return math.sqrt(math.pow((self.x-state.x),2)+
+                             math.pow((self.y - state.y),2))
 
-                return None
-            
-            def steer(self, from_node, to_node, extend_length=float("inf")):
-                
-                new_node = self.Node(from_node.x,from_node.y)
-                d, theta = self.calc_distance_and_angle(new_node, to_node)
+        def set_state(self,state):
+           if state not in ["s",".","#","e","*"]:
+               return
+           self.state = state
 
-                new_node.path_x = [new_node.x]
-                new_node.path_y = [new_node.y]
+class Map:
+    def __init__(self,row,col):
+        self.row = row
+        self.col = col
+        self.map = self.init_map()
 
-                if extend_length > d:
-                    extend_length = d
+    def init_map(self):
+        map_list = []
+        for i in range(self.col):
+            tmp = []
+            for j in range (self.col):
+                tmp.append(State(i,j))
+                map_list.append(tmp)
+            return map_list
 
-                n_expand = math.floor(extend_length / self.path_resolution)
+    def get_neighors(self, state):
+        state_list = []
+        for i in [-1,0,1]:
+            for j in [-1,0,1]:
+                if i == 0 and j == 0:
+                    continue
+                if state.x + i<0 or state.x +i >= self.row:
+                    continue
+                if state.y + j < 0 or state.y + j >= self.col:
+                    continue
+                state_list append(self.map[state.x + i][[state.y +j])
+        return state_list
 
-                for _ in range(n_expand):
-                    new_node.x += self.path_resolution * math.cos(theta)
-                    new_node.y += self.path_resolution * math.sin(theta)
-                    new_node.path_x.append(new_node.x)
-                    new_node.path_y.append(new_node.y)
-
-                d, _ = self.calc_distance_and_angle(new_node, to_node)
-                if d <= self.path_resolution:
-                    new_node.path_x.append(to_node.x)
-                    new_node.path_y.append(to_node.y)
-                    new_node.x = to_node.x
-                    new_node.y = to_node.y
-
-                new_node.parent = from_node
-
-                return new_node
-            
-
-            def generate_final_course(self, goal_ind):
-                path = [[self.end.x,self.end.y]]
-                node = self.node_list[goal_ind]
-                while node.parent is not None:
-                    path.append([node.x,node.y])
-                    node = node.parent
-                path.append([node.x,node.y])
-
-                return path
-
-            def calc_dist_to_goal(self,x,y):
-                dx = x - self.end.x
-                dy = y - self.end.y
-                return math.hypot(dx,dy)
-
-            def get_random_node(self):
-                if random.randint(0, 100) > self.goal_sample_rate:
-                    rnd = self.Node(random.uniform(self.min_rand,self.max_rand),random.uniform(self.min_rand,self.max_rand))
-                else: 
-                    rnd = self.Node(self.end.x, self.end.y)
-                return rnd
-            def draw_graph(self,rnd=None):
-                plt.clf()
-                plt.gcf().canvas.mpl_connect('key_release_event',lambda event: [exit(0) if event.key == 'escape' else None])
-                plt.plot(
+    def set_obstacle(self, point_list):
+        for x,y in point_list
