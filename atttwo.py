8368a2da6e0288073b204234f304de74f484cf5f
ayushd <dagaayush1205@gmail.com>
2024-04-08 13:00:09 +0530

    code updated

--- atttwo.py
+++ atttwo.py
@@ -1,6 +1,11 @@
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+from obs import *
+def obstacle(area):
+    obsatcles = obs.obstacles()
+    for i in len(obs):
+        if obs[i]
 def main():
     area = np.full((100,100,1),0)
     """
@@ -8,6 +13,10 @@ def main():
     1: open
     -1: obstacle
     """
-    print(area[51,54,0])
+    start = [0,0]
+    waypoint = [[90,90],[75,80],[60,70]]
+    for i in range (len(waypoint)):
+        goal = waypoint[i]
+        obstacle(area)
 if __name__=='__main__':
     main()
